b747f6dbd26afa95e17e814e88549fe164ce2409
zjhch123 <416836508@qq.com>
2020-02-03 17:25:26 +0800

    Feat: base

--- routes/wx-auth.js
+++ routes/wx-auth.js
@@ -4,10 +4,12 @@ const package = require('../package.json');
 
 const { serverURL } = package;
 
-const buildServerRedirectURI = (redirectURI) => {
+const buildServerRedirectURI = (searchParams) => {
   const baseHost = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:7610' : serverURL;
   const serverRedirectURI = new URL('/wxCodeAuth', baseHost);
-  serverRedirectURI.searchParams.append('redirect', encodeURIComponent(redirectURI));
+
+  Object.keys(searchParams).forEach(param =>
+    serverRedirectURI.searchParams.append(param, encodeURIComponent(searchParams[param])));
 
   return encodeURIComponent(serverRedirectURI.toString());
 };
@@ -16,8 +18,15 @@ module.exports = async function wxAuth (ctx, next) {
   await next();
   const {
     redirect_uri, // TODO: Will add logic of verifying the domain
+    postdata_uri,
+    error_uri,
   } = ctx.request.query;
 
-  const serverRedirectURI = buildServerRedirectURI(redirect_uri);
+  const serverRedirectURI = buildServerRedirectURI({
+    redirect: redirect_uri,
+    postdata: postdata_uri,
+    error: error_uri,
+  });
+
   ctx.redirect(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${APP_ID}&redirect_uri=${serverRedirectURI}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`);
 };
--- routes/wx-code-auth.js
+++ routes/wx-code-auth.js
@@ -1,46 +1,90 @@
-async function getAuthAccessToken (code) {
+const fetch = require('node-fetch');
+const { APP_ID, APP_SECRET, IS_DEV, JSON_CONTENT_TYPE } = require('../constants');
 
+async function getAuthAccessToken (code) {
+  return fetch(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${APP_ID}&secret=${APP_SECRET}&code=${code}&grant_type=authorization_code`)
+    .then(data => data.json())
+    .then(({ errcode, errmsg, access_token, expires_in, refresh_token, openid }) => {
+      if (errcode) { throw new Error(errmsg); }
+
+      return {
+        authAccessToken: access_token,
+        authRefreshToken: refresh_token,
+        openId: openid,
+        expire: Date.now() + expires_in * 1000,
+      };
+    });
 }
 
 async function getUserInfo ({
   authAccessToken,
-  authRefreshToken,
   openId,
 }) {
+  return fetch(`https://api.weixin.qq.com/sns/userinfo?access_token=${authAccessToken}&openid=${openId}&lang=zh_CN`)
+    .then(data => data.json())
+    .then(({ errcode, errmsg, ...userInfo }) => {
+      if (errcode) { throw new Error(errmsg); }
 
+      return userInfo;
+    });
 }
 
-async function postUserInfo (userInfo) {
-
+async function postUserInfo (postdataURI, userInfo) {
+  return fetch(postdataURI, {
+    method: 'POST',
+    headers: {
+      'Content-Type': JSON_CONTENT_TYPE,
+      Source: 'Wechat-Service',
+    },
+    body: JSON.stringify(userInfo),
+    timeout: 5000,
+  }).catch((e) => { throw new Error(`Remote server error. - \n ${e.message}`); });
 }
 
 module.exports = async function wxCodeAuth (ctx, next) {
   await next();
   const {
     code,
-    redirect,
+    redirect: redirectURI,
+    postdata: postdataURI,
+    error: errorURI,
   } = ctx.request.query;
 
-  const {
-    authAccessToken,
-    authRefreshToken,
-    openId,
-    expire,
-  } = await getAuthAccessToken(code);
+  try {
+    const {
+      authAccessToken,
+      authRefreshToken,
+      openId,
+      expire,
+    } = await getAuthAccessToken(code);
 
-  const userInfo = await getUserInfo({
-    authAccessToken,
-    authRefreshToken,
-    openId,
-  });
+    const userInfo = await getUserInfo({
+      authAccessToken,
+      openId,
+    });
 
-  await postUserInfo({
-    ...userInfo,
-    authAccessToken,
-    authRefreshToken,
-    openId,
-    expire,
-  });
+    const userPackage = {
+      ...userInfo,
+      accessToken: authAccessToken,
+      refreshToken: authRefreshToken,
+      openId,
+      expire,
+    };
 
-  ctx.redirect(redirect);
+    if (IS_DEV) {
+      await postUserInfo('https://httpbin.org/post', userPackage);
+      ctx.body = {
+        code: 200,
+        data: userPackage,
+      };
+      return;
+    }
+
+    await postUserInfo(decodeURIComponent(postdataURI), userPackage);
+
+    ctx.redirect(decodeURIComponent(redirectURI));
+  } catch (e) {
+    console.log(e);
+    errorURI && ctx.redirect(decodeURIComponent(errorURI));
+  }
 };
