1376e9cbd49f55da2e62f0e17e8c949cd88e5817
zjhch123 <416836508@qq.com>
2020-03-02 22:05:27 +0800

    Update: add tests

---
+++ src/utils/build-uri.js
@@ -0,0 +1,11 @@
+const { URL } = require('url');
+
+module.exports = function buildURI (host, path, searchParams) {
+  const baseHost = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:7610' : host;
+  const uri = new URL(path, baseHost);
+
+  Object.keys(searchParams).forEach(param =>
+    uri.searchParams.append(param, encodeURIComponent(searchParams[param])));
+
+  return uri.toString();
+};
---
+++ test/utils/build-uri.test.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+const buildURI = require('../../src/utils/build-uri');
+
+describe('build-uri.js', () => {
+  it('should return correct scripts', () => {
+    process.env.NODE_ENV = 'development';
+    expect(buildURI(null, '/a/b/c', { test: 1, age: 2, name: 3 }), 'development')
+      .to.equal('http://127.0.0.1:7610/a/b/c?test=1&age=2&name=3');
+
+    process.env.NODE_ENV = 'production';
+    expect(buildURI('http://www.baidu.com', '/d/e/f', { name: 1, age: 2, test: 3 }), 'production')
+      .to.equal('http://www.baidu.com/d/e/f?name=1&age=2&test=3');
+  });
+});
