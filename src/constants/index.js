d890d5dc43929606dce12c1f7589d42d54c5d5b5
zjhch123 <416836508@qq.com>
2020-03-09 22:14:01 +0800

    Update: test

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,19 +3,18 @@ const paths = require('../utils/paths');
 const package = require('../../package.json');
 
 const SSR_PLACEHOLDER = name => `@{ssr_${name}}`;
+const IS_DEV = process.env.NODE_ENV === 'development';
 
 let APP_ID;
 let APP_SECRET;
 
 try {
-  APP_ID = fs.readFileSync(paths.appId).toString();
-  APP_SECRET = fs.readFileSync(paths.appSecret).toString();
+  APP_ID = IS_DEV ? 'wx06f30708fbccb63e' : fs.readFileSync(paths.appId).toString();
+  APP_SECRET = IS_DEV ? '285f895ac9dd49973d36ad0c8704dfb1' : fs.readFileSync(paths.appSecret).toString();
 } catch (e) {
   APP_ID = '';
   APP_SECRET = '';
 }
-
-const IS_DEV = process.env.NODE_ENV === 'development';
 
 module.exports = {
   SSR_WX_JS: SSR_PLACEHOLDER('wxJS'),
