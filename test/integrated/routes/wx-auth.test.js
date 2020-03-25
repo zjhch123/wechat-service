4d065daab8a0d418f4947eb859e6cce975db9c9c
zjhch123 <416836508@qq.com>
2020-03-25 22:12:52 +0800

    Fix: call encodeURIComponent multi times

--- test/integrated/routes/wx-auth.test.js
+++ test/integrated/routes/wx-auth.test.js
@@ -6,7 +6,7 @@ describe('wx-auth.js', () => {
   it('should redirect to wechat auth page', async () => {
     const response = await request.get('/wxAuth?redirect_uri=https%3A%2F%2Fwww.baidu.com&postdata_uri=https%3A%2F%2Fstatic.hduzplus.xyz');
     expect(response.status).to.equal(302);
-    expect(response.header.location).to.equal(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${mockAppId}&redirect_uri=http%3A%2F%2F127.0.0.1%3A7610%2FwxCodeAuth%3Fredirect_uri%3Dhttps%25253A%25252F%25252Fwww.baidu.com%26error_uri%3Dhttps%25253A%25252F%25252Fwww.baidu.com%26postdata_uri%3Dhttps%25253A%25252F%25252Fstatic.hduzplus.xyz&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`);
+    expect(response.header.location).to.equal(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${mockAppId}&redirect_uri=http%3A%2F%2F127.0.0.1%3A7610%2FwxCodeAuth%3Fredirect_uri%3Dhttps%253A%252F%252Fwww.baidu.com%26error_uri%3Dhttps%253A%252F%252Fwww.baidu.com%26postdata_uri%3Dhttps%253A%252F%252Fstatic.hduzplus.xyz&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`);
   });
 
   it('should return error if missing params', async () => {
@@ -18,6 +18,6 @@ describe('wx-auth.js', () => {
   it('should support set custom error_uri', async () => {
     const response = await request.get('/wxAuth?redirect_uri=https%3A%2F%2Fwww.baidu.com&error_uri=http%3A%2F%2Fwww.error.com');
     expect(response.status).to.equal(302);
-    expect(response.header.location).to.equal(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${mockAppId}&redirect_uri=http%3A%2F%2F127.0.0.1%3A7610%2FwxCodeAuth%3Fredirect_uri%3Dhttps%25253A%25252F%25252Fwww.baidu.com%26error_uri%3Dhttp%25253A%25252F%25252Fwww.error.com%26postdata_uri%3Dhttps%25253A%25252F%25252Fstatic.hduzplus.xyz&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`);
+    expect(response.header.location).to.equal(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${mockAppId}&redirect_uri=http%3A%2F%2F127.0.0.1%3A7610%2FwxCodeAuth%3Fredirect_uri%3Dhttps%253A%252F%252Fwww.baidu.com%26error_uri%3Dhttp%253A%252F%252Fwww.error.com%26postdata_uri%3Dhttps%253A%252F%252Fstatic.hduzplus.xyz&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`);
   });
 });
